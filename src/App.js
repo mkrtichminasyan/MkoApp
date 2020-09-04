649d4c0df865b47ae637188b3a96686888251697
mkrtichminasyan <m-minasyan92@mail.ru>
2020-09-04 18:44:54 +0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/header'
+import Footer from './Components/footer'
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Header</h1>
+        <ul>
+            <li>Facebook</li>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
